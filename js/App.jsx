0ac93c15895131185a9e2931e3fc9ab07ea5dd30
Kevin Silvester <kevinsilvester05@gmail.com>
2021-10-20 20:03:45 +0100

    finished making the search

--- js/App.jsx
+++ js/App.jsx
@@ -1,57 +1,70 @@
 const { useState, useEffect } = React;
 
 const App = () => {
-	const [fetchInfo, setFetchInfo] = useState({
-		error: null,
-		loaded: false,
-		data: [],
-	});
+   const [fetchInfo, setFetchInfo] = useState({ error: null, loaded: false, data: [] });
+   const [filterData, setFilterData] = useState([])
+   const [displayData, setDisplayData] = useState([])
 
-	useEffect(() => {
-		fetch("https://failteireland.azure-api.net/opendata-api/v1/attractions")
-			.then((res) => res.json())
-			.then((jsonData) => {
-				let temp = [];
-				jsonData.results.map((e) => {
-					const place = {
-						name: e.name,
-						type: e["@type"],
-						address: {
-							addressLocality: e.address.addressLocality,
-							addressRegion: e.address.addressRegion,
-						},
-						coordinates: {
-							latitude: e.geo.latitude,
-							longitude: e.geo.longitude,
-						},
-						image: e.image.url,
-						tags: e.tags,
-						phone: e.telephone,
-						website: e.url,
-					};
-					temp.push(place);
-				});
-				setFetchInfo({ error: null, loaded: true, data: temp });
-			})
-			.catch((err) => setFetchInfo({ error: err, loaded: true, data: [] }));
-	}, []);
+   useEffect(() => {
+      fetch("https://failteireland.azure-api.net/opendata-api/v1/attractions")
+         .then(res => res.json())
+         .then(jsonData => {
+            let temp = [];
+            jsonData.results.map((e) => {
+               const place = {
+                  name: e.name,
+                  type: e["@type"],
+                  address: {
+                     addressLocality: e.address.addressLocality,
+                     addressRegion: e.address.addressRegion,
+                  },
+                  coordinates: {
+                     latitude: e.geo.latitude,
+                     longitude: e.geo.longitude,
+                  },
+                  image: e.image.url,
+                  tags: e.tags,
+                  phone: e.telephone,
+                  website: e.url,
+               };
+               temp.push(place);
+            });
+            temp.sort((a, b) => (a.name < b.name ? -1 : 1))
+            setFetchInfo({ error: null, loaded: true, data: temp });
+            setFilterData(temp)
+            setDisplayData(temp)
+         })
+         .catch(err => setFetchInfo({ error: err, loaded: true, data: [] }));
+   }, []);
+
+   const handleFilterSort = (data, sortVal) => {
+
+   }
+
+   const handleSearch = searchTerm => {
+      if (searchTerm.length === 0) {
+         setDisplayData(filterData)
+         return
+      }
+      setDisplayData([...filterData.filter(place => place.name.toLowerCase().match(new RegExp(`${searchTerm.toLowerCase()}`)))])
+   }
 
    if (fetchInfo.error) return <div>Fetch Failed</div>
 
    if (!fetchInfo.loaded) return <div>Loading....</div>
 
-	return (
-		<div>
-         <Search />
-			<Table data={fetchInfo.data} />
-		</div>
-	);
+   return (
+      <div>
+         <Search searchTerm={handleSearch} />
+         <Table data={displayData} />
+      </div>
+   );
 };
 
 ReactDOM.render(
-	<React.StrictMode>
+   <React.StrictMode>
       <App />
-	</React.StrictMode>,
-	document.getElementById("root")
+   </React.StrictMode>,
+   document.getElementById("root")
 );
 
--- js/components/Search.jsx
+++ js/components/Search.jsx
@@ -1,9 +1,21 @@
-
 const Search = props => {
-   const [val, setVal] = useState('')
+   const [val, setVal] = useState("");
+
+   const searchTerm = props.searchTerm;
+
+   useEffect(() => searchTerm(val), [val]);
+
    return (
-      <div>
-         <input type="text" />
+      <div className="search__container">
+         <input
+            className="search__input"
+            type="text"
+            onChange={e => setVal(e.target.value)}
+            value={val}
+         />
+         <button className="search__clear" onClick={() => setVal("")}>
+            <i className="fas fa-times"></i>
+         </button>
       </div>
-   )
-}
+   );
+};
